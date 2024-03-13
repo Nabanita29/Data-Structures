fc7fca93cda033ae86939cef7e9453f39e0394f9
Nabanita <107246882+Nabanita29@users.noreply.github.com>
2024-03-13 11:50:33 +0530

    Time: 1 ms (77.54%) | Memory: 41 MB (17.72%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.java
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -1,30 +1,15 @@
 class Solution {
     public int pivotInteger(int n) {
-        int p = 0;
-        int s = 0;
+        int[] prefs = new int[n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            prefs[i] = prefs[i - 1] + i;
+        }
 
-        for (int i = 1; i <= n; i++) {
-            if (prefixSum(0, i) == suffixSum(i, n)) {
+        for (int i = 1; i < n + 1; i++) {
+            if (prefs[i] == prefs[n] - prefs[i - 1])
                 return i;
-            }
         }
 
         return -1;
-    }
-
-    private int prefixSum(int y, int z) {
-        int p = 0;
-        for (int i = y; i <= z; i++) {
-            p = p + i;
-        }
-        return p;
-    }
-
-    private int suffixSum(int k, int l) {
-        int s = 0;
-        for (int i = k; i <= l; i++) {
-            s = s + i;
-        }
-        return s;
     }
 }
