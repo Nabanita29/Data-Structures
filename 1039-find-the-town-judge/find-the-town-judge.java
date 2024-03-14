94d3c5167535a3e968f9e7a01d25aa73e91d7ba9
Nabanita <107246882+Nabanita29@users.noreply.github.com>
2024-03-14 21:27:21 +0530

    Time: 2 ms (99.61%) | Memory: 53.5 MB (70.09%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -1,27 +1,19 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        if (n == 1) return 1;
-        // Make one hashset and one array and keep a count of trust[0] = person who trusts
-        // trust[1] = person trusted by everyone
-        // Iterate both and see which person is trusted by everyone
-        HashSet<Integer> trustsSomeone = new HashSet<>();
-        int[] trustCounts = new int[n + 1]; // We use n + 1 because the labels are from 1 to n
-
-        for (int[] pair : trust) {
-            int person = pair[0];
-            int trustedPerson = pair[1];
-            
-            trustsSomeone.add(person); // Record that this person trusts someone
-            trustCounts[trustedPerson]++; // Increment the trust count for the trusted person
+        int in[]=new int[n+1];
+        int out[]=new int[n+1];
+        for(int k[]:trust){
+            out[k[0]]++;
+            in[k[1]]++;
         }
-        for(int i = 0; i <= n; i++){
-            // Now the town judge SHOULD NOT BE in the trusts someone
-            // and he should have appeared n - 1 times in the array (1,2,3) 1 is townjudge so 2 is not
-            if(!trustsSomeone.contains(i) && trustCounts[i] == n-1){
+
+        for(int i=1;i<=n;i++){
+            if(in[i]==n-1 && out[i]==0){
                 return i;
             }
         }
         return -1;
-        
+
+
     }
 }
